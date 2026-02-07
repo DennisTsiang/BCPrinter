9b5c915451ec1b0cd737eb8b734f00c11fe03110
Dennis Tsiang <24190844+DennisTsiang@users.noreply.github.com>
2026-02-07 13:39:02 +0000

    Update version to 2.3.0

--- BCPrinter.py
+++ BCPrinter.py
@@ -418,7 +418,7 @@ window_size=(500, 800)
 if debug_mode or browser_mode:
     window_size = None
 ui.run(root=root,
-       title="Royal Papworth Hospital Barcode Printing Ver (2.2.0)",
+       title="Royal Papworth Hospital Barcode Printing Ver (2.3.0)",
        window_size=window_size,
        favicon="🖨️",
        reload=debug_mode)
